0fa612a8e90aa80b965dbc40db158b40f393fb41
Pasang-lama <lpasang182@gmail.com>
2024-08-26 16:30:25 +0545

    works on the carousel slider

--- src/component/Testimonals.jsx
+++ src/component/Testimonals.jsx
@@ -6,22 +6,24 @@ import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
   superLargeDesktop: {
+    // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
-    items: 1,
+    items: 1
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1,
+    items: 1
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 1,
+    items: 1
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
+    items: 1
+  }
 };
+
 
 const Stories = () => {
   const reviews = [
@@ -46,14 +48,9 @@ const Stories = () => {
       <div className="content ">
         <div className="top-cloud-wrapper">
           <img src={cloud1} className=" " alt="cloud" />
-          <img src={cloud1} className=" " alt="cloud" />
-          <img src={cloud1} className=" " alt="cloud" />
-          <img src={cloud1} className=" " alt="cloud" />
-          <img src={cloud1} className=" " alt="cloud" />
-          <img src={cloud1} className=" " alt="cloud" />
         </div>
 
-        <div className="container d-flex">
+        <div className="container testimonails-wrapper">
           <div className="glimpse d-flex">
             <h4 className="heading">
               Glimse of Inspiring <span className="stories">stories</span>
@@ -89,28 +86,6 @@ const Stories = () => {
             ))}
           </Carousel>
     
-          {/* <div className="review d-flex">
-            <i className="fa-solid fa-quote-right"></i>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-              atque nesciunt necessitatibus officia, tempora accusantium, iusto
-              alias blanditiis facere voluptatibus obcaecati vitae iste
-              doloribus eaque. Nulla facere suscipit culpa Lorem ipsum dolor sit
-              amet consectetur, adipisicing elit. Libero, hic!
-            </p>
-            <div className="identity d-flex">
-              <div className="reviewer-img">
-                <img
-                  src={profile}
-                  alt="profile"
-                />
-                </div>
-                <div className="name d-flex">
-                  <p className="heading">Leslie Alexander</p>
-                  <p className="paragraph">Canada</p>
-                </div>
-            </div>
-          </div> */}
         </div>
 
         <img src={cloud2} className="cloud2" alt="cloud" />
