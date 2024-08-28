3523cc7b5c2537232e326696a365aabe45a866d4
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-08-28 12:41:16 +0545

    landing page complete

--- src/component/BreadcrumbBanner.jsx
+++ src/component/BreadcrumbBanner.jsx
@@ -5,15 +5,18 @@ const BreadcrumbBanner = () => {
     <>
     <Navbar/>
     <section className="breadcrumb-banner">
-      <h3 className="heading">Gokyo Peak Travels and Tourism Pvt. Ltd.</h3>
-
-      <nav className='breadcrumb d-flex'>
-          <ul>
-            <li className='breadcrumb-item'><a href="#">Home</a></li>
-            <li className='breadcrumb-item'><a href="#">About Us</a></li>
+        <h3 className="heading">Gokyo Peak Travels and Tourism Pvt. Ltd.</h3>
+        <nav className="breadcrumb ">
+          <ul className="d-flex">
+            <li className="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li className="breadcrumb-item">
+              <a href="#">About Us</a>
+            </li>
           </ul>
-      </nav>
-    </section>
+        </nav>
+      </section>
     </>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,16 @@
-import Navbarr from "../component/Navbar";
 import Footer from "../component/Footer";
+import BreadcrumbBanner from "../component/BreadcrumbBanner";
+import Aboutus from "../component/Aboutus";
+import Customize from "../component/Customize";
+import Association from "../component/Association";
 
 function About() {
   return (
     <>
-      <Navbarr />
-      <section className="breadcrumb-banner">
-        <h3 className="heading">Gokyo Peak Travels and Tourism Pvt. Ltd.</h3>
-        <nav className="breadcrumb ">
-          <ul className="d-flex">
-            <li className="breadcrumb-item">
-              <a href="#">Home</a>
-            </li>
-            <li className="breadcrumb-item">
-              <a href="#">About Us</a>
-            </li>
-          </ul>
-        </nav>
-      </section>
+      <BreadcrumbBanner/>
+      <Aboutus/>
+      <Customize/>
+      <Association/>
       <Footer/>
     </>
   );
